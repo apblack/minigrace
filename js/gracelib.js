b359f7e5a7ca18b3cf265e28a62ccbde63f82d3a
Andrew P. Black <black@cs.pdx.edu>
2016-09-06 08:21:05 -0700

    Adds method to build Alias objects.
    That's all.

--- js/gracelib.js
+++ js/gracelib.js
@@ -2233,8 +2233,8 @@ function gracecode_io() {
     this.methods['listdir(1)'] = function (argcv, x) {
         if(typeof(process) !== "undefined") {
             var list = [];
-	    list.push(new GraceString("."));
-	    list.push(new GraceString(".."));
+    list.push(new GraceString("."));
+    list.push(new GraceString(".."));
             fs.readdirSync(safeJsString(x)).forEach(function(val, index, array) {
                 list.push(new GraceString(val));
             });
@@ -2257,7 +2257,7 @@ function gracecode_sys() {
         if(typeof(process) !== "undefined") {
             var list = [];
             process.argv.forEach(function(val, index, array) {
-		        if(index > 1)
+        if(index > 1)
                     list.push(new GraceString(val));
             });
             return new GraceList(list);
@@ -2267,7 +2267,7 @@ function gracecode_sys() {
                 new GraceString("--target"),
                 new GraceString("js")
             ]);
-	    }
+    }
     };
     this.methods.elapsed = function() {
         return new GraceNum(((new Date).getTime()/1000)-startTime);
@@ -3257,6 +3257,11 @@ function ReturnException(v, target) {
 
 ReturnException.prototype = {
     'exctype': 'return'
+};
+
+function Alias(newName, oldName) {
+    this.newName = newName;
+    this.oldName = oldName;
 };
 
 function GraceExceptionPacket(exception, message, data) {
