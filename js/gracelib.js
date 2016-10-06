b91487817643ad235a5ebc6387ecb1080a48220e
Andrew P. Black <black@cs.pdx.edu>
2016-10-06 16:31:30 -0700

    Defines `pathname` of a non-file stream to be "".
    Formerly, the C library and the JS library behaved differently
    in this case.

--- js/gracelib.js
+++ js/gracelib.js
@@ -2025,6 +2025,7 @@ var stdout = Grace_allocObject(GraceObject, "stdout");
 stdout.methods.write = function(junk, s) {
     minigrace.stdout_write(s._value);
 };
+stdout.methods.pathname = function() { return new GraceString(""); };
 stdout.methods.isatty = function() {
         if(typeof(process) !== "undefined") {
             return Boolean(process.stdout.isTTY) ? GraceTrue : GraceFalse;
@@ -2054,6 +2055,7 @@ stdin.methods.isatty = function() {
             return GraceFalse;
         }
 };
+stdin.methods.pathname = function() { return new GraceString(""); };
 stdin.methods.close = function() {};
 stdin.methods['==(1)'] = function (argcv, other) {
     return (this===other) ? GraceTrue : GraceFalse;
@@ -2072,6 +2074,7 @@ stderr.methods.isatty = function() {
         return GraceFalse;
     }
 };
+stderr.methods.pathname = function() { return new GraceString(""); };
 stderr.methods.close = function() {};
 stderr.methods['==(1)'] = function (argcv, other) {
     return (this===other) ? GraceTrue : GraceFalse;
